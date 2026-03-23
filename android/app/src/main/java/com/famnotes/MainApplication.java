6c3426fd29cbfff1be52ebe8aca8b0fa04aacc47
mixailksk-cyber <mixailksk@gmail.com>
2026-03-23 09:12:56 +0000

    Simplify MainApplication without PackageList

--- android/app/src/main/java/com/famnotes/MainApplication.java
+++ android/app/src/main/java/com/famnotes/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -20,7 +21,8 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-          return new PackageList(this).getPackages();
+          List<ReactPackage> packages = new ArrayList<>();
+          return packages;
         }
 
         @Override
