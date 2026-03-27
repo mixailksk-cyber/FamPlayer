67e3dea7eca8940f0072e05a2c59aa74586d0ea7
mixailksk-cyber <mixailksk@gmail.com>
2026-03-27 10:00:28 +0500

    Update FamNotesWidgetProvider.java

--- android/app/src/main/java/com/famnotes/FamNotesWidgetProvider.java
+++ android/app/src/main/java/com/famnotes/FamNotesWidgetProvider.java
@@ -3,6 +3,7 @@ package com.famnotes;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
