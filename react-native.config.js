6dc912c0da66954f71563df81a0be8ebb0e4c3f1
mixailksk-cyber <mixailksk@gmail.com>
2026-04-01 09:53:07 +0000

    fix: disable autolinking, use manual includes

---
+++ react-native.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        android: null, // disable Android platform, other platforms will still autolink if provided
+      },
+    },
+    'react-native-push-notification': {
+      platforms: {
+        android: null,
+      },
+    },
+    'react-native-fs': {
+      platforms: {
+        android: null,
+      },
+    },
+    'react-native-document-picker': {
+      platforms: {
+        android: null,
+      },
+    },
+    'react-native-orientation-locker': {
+      platforms: {
+        android: null,
+      },
+    },
+    'react-native-safe-area-context': {
+      platforms: {
+        android: null,
+      },
+    },
+    'react-native-share': {
+      platforms: {
+        android: null,
+      },
+    },
+  },
+};
